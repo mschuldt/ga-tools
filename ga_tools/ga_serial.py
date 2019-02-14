bd0cff622b8ef3ddfa818d36368b17f9f4b0b328
mschuldt <mbschuldt@gmail.com>
2019-02-14 00:06:11 -0800

    method to collect received serial data

--- ga_tools/ga_serial.py
+++ ga_tools/ga_serial.py
@@ -6,6 +6,9 @@ class GA144Serial:
         self.serial = Serial(port, speed)
         self.serial.reset_input_buffer()
 
+        self.data = []
+        self.errors = []
+
     def write_bootstream(self, stream):
         #if target:
         #    target.setRTS(0)
@@ -35,3 +38,16 @@ class GA144Serial:
                 print(n & 0x3ffff)
             else:
                 raise Exception('unknown serial code: '+str(n))
+
+    def gather(self):
+        self.data = []
+        self.errors = []
+        while True:
+            n = self.read_n(1)
+            if n == 1:
+                return
+            if n == 0:
+                n = self.read_n(3)
+                self.data.append(n & 0x3ffff)
+            else:
+                self.errors.append(n)
