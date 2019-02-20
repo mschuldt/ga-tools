4f60b8cb5062d40f199bcb01174c9ba6b95af5fe
mschuldt <mbschuldt@gmail.com>
2019-02-20 01:29:24 -0800

    fix

--- ga_tools/__init__.py
+++ ga_tools/__init__.py
@@ -11,7 +11,7 @@ def chip_json(chip, bootstream_type=None, simulation=False):
     data = {'nodes': {coord:node.json() for coord, node
                       in chip.nodes.items()}}
     if bootstream_type:
-        bs = ga_tools.make_bootstream(bootstream_type, chip)
+        bs = make_bootstream(bootstream_type, [chip])
         data['bootstream'] = {'type':bootstream_type,
                               'node': bs.start_coord,
                               'stream': bs.stream(not simulation)}
